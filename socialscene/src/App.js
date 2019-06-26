cd7c4c038b41d1785c4c708b0c1ff1af2f90db08
Simon Barer <simonbarer@gmail.com>
2019-06-26 12:13:42 -0700

    starting implementation of facebook login

--- socialscene/src/App.js
+++ socialscene/src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hi
+          <div class="fb-login-button" 
+            data-width="" 
+            data-size="large" 
+            data-button-type="continue_with" 
+            data-auto-logout-link="false" 
+            data-use-continue-as="true">
+          </div>
         </p>
       </header>
     </div>
