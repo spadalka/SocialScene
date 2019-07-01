2dae265fc4701d32e7908240f28060f896be791e
Simon Barer <simonbarer@gmail.com>
2019-06-30 21:20:15 -0700

    facebook button testing

--- socialscene/src/App.js
+++ socialscene/src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import fb from './components/Facebook';
 
-function App() {
+class App extends Component { 
+  render() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>SocialScene</h1>
         <p>
-          <div class="fb-login-button" 
+        To get started, <br/>Please login with your Facebook Profile.
+        <br/><br/>
+        {/* <FacebookLoginButton className="fb-login-button" 
             data-width="" 
             data-size="large" 
             data-button-type="continue_with" 
             data-auto-logout-link="false" 
-            data-use-continue-as="true">
-          </div>
-        </p>
+            data-use-continue-as="true"
+            /> */}
+      </p>
+      {/* <fb:login-button scope="public_profile" onlogin="checkLoginState();" /> */}
+      <div className="fb-login-button" 
+          data-width="" 
+          data-size="large" 
+          data-button-type="continue_with" 
+          data-auto-logout-link="false" 
+          data-use-continue-as="true">
+      </div>
       </header>
     </div>
   );
-}
+};
+};
 
 export default App;
