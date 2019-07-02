dc6e480d925f15096b040c95afe71f87ce6a832b
Abel Thomas <abelathomas@gmail.com>
2019-07-02 10:07:20 -0700

    connecting pg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 import * as serviceWorker from './serviceWorker';
+
+const app = express()
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
