04edb54114fbd6bf14fbc5d909f1b8c12233873d
Abel Thomas <abelathomas@gmail.com>
2019-07-13 18:59:45 -0700

    db link

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: true
-  user: 'postgres',
-  password: 'root',
-  host: 'localhost',
-  database: 'postgres'
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+  // user: 'postgres',
+  // password: 'root',
+  // host: 'localhost',
+  // database: 'postgres'
 });
 
 
