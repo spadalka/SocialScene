64993f9c7fe1f1daaeee873e53d49e8f7c04d726
Johnson <jta118@sfu.ca>
2019-07-13 17:36:56 -0700

    remember to change db to heroku

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const PORT = process.env.PORT || 5000
 const request = require("request")
 const { Pool } = require('pg');
 const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: true
-  user: 'postgres',
-  password: 'pgsqlsucks',
-  host: 'localhost',
-  database: 'postgres'
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+  // user: 'postgres',
+  // password: 'pgsqlsucks',
+  // host: 'localhost',
+  // database: 'postgres'
 });
 
 
